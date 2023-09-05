8103b400af462f3b7d4d846c2c4a3fa5f9cf66f5
Dave <shrps@posteo.net>
2023-09-05 11:25:52 +0200

    pipe file

---
+++ pipe.py
@@ -0,0 +1,18 @@
+import dlt
+
+
+data = [
+    {'id': 1, 'name': 'Alice'},
+    {'id': 2, 'name': 'Bob'}
+]
+
+
+pipeline = dlt.pipeline(
+            pipeline_name="some_pipeline",
+            dataset_name="some_dataset",
+            staging="filesystem",
+            destination="bigquery",
+        )
+
+load_info = pipeline.run(data, table_name="users", write_disposition="replace")
+print(load_info)
